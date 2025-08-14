499f7d8620035b5a1d0b6f1376bd24436badc866
alaaabdelhay <alaaabdelhay65@gmail.com>
2025-08-14 13:34:56 +0300

    TASK 15

---
+++ Task_15/eclipse_code/KEYPAD_interface.h
@@ -0,0 +1,8 @@
+#ifndef KEYPAD_INTERFACE_H_
+#define KEYPAD_INTERFACE_H_
+
+uint8 KPD_GetPressedKey(void);
+
+void KPD_Init(void);
+
+#endif /* KEYPAD_INTERFACE_H_ */
---
+++ Task_15/eclipse_code/STD_TYPES.h
@@ -0,0 +1,19 @@
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+typedef unsigned char uint8;
+typedef signed char sint8;
+
+typedef unsigned short int uint16;
+typedef signed short int sint16;
+
+typedef unsigned long int uint32;
+typedef signed long int sint32;
+
+typedef unsigned long long int uint64;
+typedef signed long long int sint64;
+
+typedef float float32;
+typedef double float64;
+
+#endif /* STD_TYPES_H_ */
